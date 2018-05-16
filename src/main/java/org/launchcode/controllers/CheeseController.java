88183eb23d056f86a14046426a442250fed5e223
Spillz <billygcasey@hotmail.com>
2018-05-16 08:39:30 -0500

    Intro to Spring Boot: Views (Part 2)

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -3,6 +3,11 @@ package org.launchcode.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -12,11 +17,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "cheese")  // Makes requesse path localhost-port/cheese
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     // Request path: cheese/
     @RequestMapping(value = "")
     public String index(Model model) {
 
+        // ArrayList<String> cheeses = new ArrayList<>(); This moved above adding "static" proceding it
+        /* cheeses.add("cheddar");
+        cheeses.add("Sharp");
+        cheeses.add("Extra Sharp"); removed static cheeses list */
+
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");  //this passes info into the index template
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String proceessAddCheeseform(@RequestParam String cheeseName) {
+    /*  Alt way to do line above
+    public String proceessAddCheeseform(HttpServletRequest request) {
+        String cheeseName = request.getParameter("cheeseName"); */
+        cheeses.add(cheeseName);
+        //redirectes to cheese handler
+        return "redirect:";
+
+
+
     }
 }
