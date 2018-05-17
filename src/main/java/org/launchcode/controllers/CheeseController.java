e4fcabb595579fc0887f8fe07b2844779b3c527d
Spillz <billygcasey@hotmail.com>
2018-05-16 20:22:40 -0500

    getting some descriptions worksing. Saving befor changing to hash

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -18,7 +19,7 @@ import java.util.ArrayList;
 public class CheeseController {
 
     static ArrayList<String> cheeses = new ArrayList<>();
-    static ArrayList<String> cheese_Discription = new ArrayList<>();
+    static ArrayList<String> cheese_Discription = new ArrayList<>(); //added this line to get desctiptions
 
 
     // Request path: cheese/
@@ -31,6 +32,7 @@ public class CheeseController {
         cheeses.add("Extra Sharp"); removed static cheeses list */
 
         model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheese_Discription", cheese_Discription); //added this line to get desctiptions
         model.addAttribute("title", "My Cheeses");  //this passes info into the index template
         return "cheese/index";
     }
@@ -43,15 +45,26 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String proceessAddCheeseform(@RequestParam String cheeseName) {
+    public String proceessAddCheeseform(@RequestParam String cheeseName, String cheese_disc) {
     /*  Alt way to do line above
     public String proceessAddCheeseform(HttpServletRequest request) {
         String cheeseName = request.getParameter("cheeseName"); */
         cheeses.add(cheeseName);
+        cheese_Discription.add(cheese_disc);   //added this line to get desctiptions
         //redirectes to cheese handler
         return "redirect:";
 
+    }
 
+    //Studio, adding crossoff
+    /*
+    @RequestMapping(value = "crossoff", method = RequestMethod.GET)
+    public String displayCrossoffCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/crossoff";
 
     }
+    */
+
+
 }
